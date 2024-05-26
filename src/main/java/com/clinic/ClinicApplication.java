f32bf123daba14bfb513ba477c6a06a967465beb
cuihairu <chuihairu@gmail.com>
2024-05-26 13:41:13 +0800

    fix db url

--- src/main/java/com/clinic/ClinicApplication.java
+++ src/main/java/com/clinic/ClinicApplication.java
@@ -3,11 +3,12 @@ package com.clinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EnableCaching
 @SpringBootApplication(scanBasePackages = "com.clinic.*")
-
+@EnableJpaRepositories(basePackages = {"com.clinic.repository"})
 public class ClinicApplication {
 
 	public static void main(String[] args) {
